06ebb60125231f1661b36b3cba5a56ade3256a6e
DJIEUGA <djieugadjoubirylenathan@gmail.com>
2022-10-24 07:10:21 -0700

    2-strlen.c created

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - compute nu,mber od characters in string.
+ * @s: String param.
+ * Return: Integer (string length).
+ */
+int _strlen(char *s)
+{
+	int len = strlen(*s);
+	return len;
+}
