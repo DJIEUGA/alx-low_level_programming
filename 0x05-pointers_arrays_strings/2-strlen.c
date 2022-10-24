e1992cd654ce4a8233d28f31e14e604ed971b8e2
DJIEUGA <djieugadjoubirylenathan@gmail.com>
2022-10-24 07:39:19 -0700

    2-strlen.c modified

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,12 +3,12 @@
 #include <string.h>
 
 /**
- * _strlen - compute nu,mber od characters in string.
+ * _strlen - compute number od characters in string.
  * @s: String param.
  * Return: Integer (string length).
  */
 int _strlen(char *s)
 {
 	int len = strlen(s);
-	return len;
+	return (len);
 }
