f6b77a23e1653922cf624106cf1e5ec9bca571b8
DJIEUGA <djieugadjoubirylenathan@gmail.com>
2022-10-24 14:19:59 -0700

    main.h modified and 3-puts.c created

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Print string to stdout.
+ * @str : Char Pointer parameter.
+ * Return - Void.
+ */
+
+void _puts(char *str)
+{
+	char *str;
+
+	puts(str);
+}
