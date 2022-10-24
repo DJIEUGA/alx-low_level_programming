a38fba2a6bb51148fcfd4940b3dd0863e4b33a9a
DJIEUGA <djieugadjoubirylenathan@gmail.com>
2022-10-24 14:58:49 -0700

    4-print_rev.c modified

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - Print string in reverse order.
+ * @s : pointer to char param.
+ * Return - Void.
+ */
+
+void print_rev(char *s)
+{
+	strrev(s);
+}
